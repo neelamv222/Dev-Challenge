583b164db87e4d7a6bea0265b98183b9ccdb1412
Adam Iley <adam.iley@db.com>
2016-05-09 14:41:39 +0100

    Initial check in

---
+++ Readme.md
@@ -0,0 +1,13 @@
+DB Updating Table
+=================
+
+This module contains a recruitment test for DB.
+
+The instructions are in the site/index.html file.
+
+To view them, run `npm start` from within this directory.  This
+will start a development server (using webpack) that supports hot reloading,
+but also provides a stomp/ws endpoint providing fake fx updates.
+
+Once you've started the development server, navigate to http://localhost:8011
+to read the task description and get started.
---
+++ index.js
@@ -0,0 +1,29 @@
+/**
+ * This javascript file will constitute the main part of your solution.
+ *
+ * Edit it as you need.  It currently contains things that you might find helpful to get started.
+ */
+
+// This is not really required, but means that changes to index.html will cause a reload.
+require('./site/index.html')
+// Apply the styles in style.css to the page.
+require('./site/style.css')
+
+// Change this to get detailed logging from the stomp library
+global.DEBUG = false
+
+const url = "ws://localhost:8011/stomp"
+const client = Stomp.client(url)
+client.debug = function(msg) {
+  if (global.DEBUG) {
+    console.info(msg)
+  }
+}
+
+function connectCallback() {
+  document.getElementById('stomp-status').innerHTML = "It has now successfully connected to a stomp server serving price updates for some foreign exchange currency pairs."
+}
+
+client.connect({}, connectCallback, function(error) {
+  alert(error.headers.message)
+})
